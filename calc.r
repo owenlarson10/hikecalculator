c94e9849163330776f5aa62fa1b596f0ad3119ad
Owen Larson <Owen.larson10@gmail.com>
2021-10-23 14:10:35 -0400

    added if/else for uphill and downhill

--- calc.r
+++ calc.r
@@ -10,12 +10,14 @@ t <- as.numeric(readline(prompt = "Enter Terrain Factor: "))
 g <- as.numeric(readline(prompt = "Enter Grade (Percent): "))
 s <- as.numeric(readline(prompt = "Enter Walking Speed: "))  
 
-U <- w/2.2*60/4184 * 20.1*(3.05 + (w/2.2 + L/2.2) / w*2.2 * t * (0.32 * g + 3.28 +
+if (g >= 0) {
+  U <- w/2.2*60/4184 * 20.1*(3.05 + (w/2.2 + L/2.2) / w*2.2 * t * (0.32 * g + 3.28 +
            (1 + 0.19 * g) * 2.66 * (s*1609/3600)*(s*1609/3600)))
-U  
+  cat("Calories burned: ", as.character(round(U, digits = 2)))  
+} else {
   
-D <- w/2.2*60/4184 * 20.1*(3.05 + 0.73 * (w/2.2 + L/2.2) / w*2.2 * t * 
+  D <- w/2.2*60/4184 * 20.1*(3.05 + 0.73 * (w/2.2 + L/2.2) / w*2.2 * t * 
                              (3.28 + 2.66 * (s*1609/3600)*(s*1609/3600)))
-D
-
+  cat("Calories Burned hiking downhill: ", as.character(round(D, digits = 2)))
+}
 #vg <- vr + (c1 * g) + vwm + (1 + (c2 * g)) * (c3 * v^2)
